df4fc5c0dbd3aa1a1c1734d8ce230a8d7faf0e83
doricigor <igor@forwardslashny.com>
2020-09-15 19:16:17 +0200

    Footer

--- footer.php
+++ footer.php
@@ -1,5 +1,139 @@
 <footer class="footer">
-
+    <div class="container footer__container">
+        <div class="footer__wrap">
+            <div class="row">
+                <div class="col-lg-5 footer__box-wrap">
+                    <div class="footer__box-content">
+                        <a class="footer__box-content-logo" href="javascript:;">MoGo</a>
+                        <div class="entry-content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                        </div>
+                    </div>
+                    <div class="footer__box-social">
+                        <span class="footer__box-social-followers"><strong>15k</strong> followers</span>
+                        <div class="footer__box-social-icon-wrap">
+                            <span class="footer__box-social-icon-title">Follow Us:</span>
+                            <ul class="footer__box-social-icon-list">
+                                <li class="footer__box-social-icon-list-item">
+                                    <a class="footer__box-social-icon-list-item-link" href="javascript:;"></a>
+                                </li>
+                                <li class="footer__box-social-icon-list-item">
+                                    <a class="footer__box-social-icon-list-item-link" href="javascript:;"></a>
+                                </li>
+                                <li class="footer__box-social-icon-list-item">
+                                    <a class="footer__box-social-icon-list-item-link" href="javascript:;"></a>
+                                </li>
+                                <li class="footer__box-social-icon-list-item">
+                                    <a class="footer__box-social-icon-list-item-link" href="javascript:;"></a>
+                                </li>
+                                <li class="footer__box-social-icon-list-item">
+                                    <a class="footer__box-social-icon-list-item-link" href="javascript:;"></a>
+                                </li>
+                                <li class="footer__box-social-icon-list-item">
+                                    <a class="footer__box-social-icon-list-item-link" href="javascript:;"></a>
+                                </li>
+                                <li class="footer__box-social-icon-list-item">
+                                    <a class="footer__box-social-icon-list-item-link" href="javascript:;"></a>
+                                </li>
+                                <li class="footer__box-social-icon-list-item">
+                                    <a class="footer__box-social-icon-list-item-link" href="javascript:;"></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="footer__box-social-news">
+                            <input class="footer__box-social-news-emil" type="email" placeholder="Your Email...">
+                            <input class="footer__box-social-news-sub" type="submit" value="SUBSCRIBE">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 footer__blog-wrap">
+                    <span class="footer__blog-title">Blogs</span>
+                    <div class="footer__blog-item-wrap">
+                        <div class="footer__blog-item">
+                            <div class="footer__blog-item-img-wrap">
+                                <img src="img/footer-blog-1.jpg" alt="Img" class="footer__blog-item-img">
+                            </div>
+                            <div class="footer__blog-item-content">
+                                <a class="footer__blog-item-content-link" href="javascript:;">Lorem ipsum dolor sit amet, consectetur adipiscing</a>
+                                <span class="footer__blog-item-content-date">Jan 9, 2016</span>
+                            </div>
+                        </div>
+                        <div class="footer__blog-item">
+                            <div class="footer__blog-item-img-wrap">
+                                <img src="img/footer-blog-2.jpg" alt="Img" class="footer__blog-item-img">
+                            </div>
+                            <div class="footer__blog-item-content">
+                                <a class="footer__blog-item-content-link" href="javascript:;">Lorem ipsum dolor sit amet, consectetur adipiscing</a>
+                                <span class="footer__blog-item-content-date">Jan 9, 2016</span>
+                            </div>
+                        </div>
+                        <div class="footer__blog-item">
+                            <div class="footer__blog-item-img-wrap">
+                                <img src="img/footer-blog-3.jpg" alt="Img" class="footer__blog-item-img">
+                            </div>
+                            <div class="footer__blog-item-content">
+                                <a class="footer__blog-item-content-link" href="javascript:;">Lorem ipsum dolor sit amet, consectetur adipiscing</a>
+                                <span class="footer__blog-item-content-date">Jan 9, 2016</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 footer__gallery-wrap">
+                    <span class="footer__gallery-title">Instagram</span>
+                    <div class="footer__gallery-inner">
+                        <div class="footer__gallery-img-wrap">
+                            <a class="footer__gallery-img-link" href="img/footer-instagram-1.jpg" data-fancybox="gallery">
+                                <img class="footer__gallery-img" src="img/footer-instagram-1.jpg" alt="Image">
+                            </a>
+                        </div>
+                        <div class="footer__gallery-img-wrap">
+                            <a class="footer__gallery-img-link" href="img/footer-instagram-2.jpg" data-fancybox="gallery">
+                                <img class="footer__gallery-img" src="img/footer-instagram-2.jpg" alt="Image">
+                            </a>
+                        </div>
+                        <div class="footer__gallery-img-wrap">
+                            <a class="footer__gallery-img-link" href="img/footer-instagram-3.jpg" data-fancybox="gallery">
+                                <img class="footer__gallery-img" src="img/footer-instagram-3.jpg" alt="Image">
+                            </a>
+                        </div>
+                        <div class="footer__gallery-img-wrap">
+                            <a class="footer__gallery-img-link" href="img/footer-instagram-4.jpg" data-fancybox="gallery">
+                                <img class="footer__gallery-img" src="img/footer-instagram-4.jpg" alt="Image">
+                            </a>
+                        </div>
+                        <div class="footer__gallery-img-wrap">
+                            <a class="footer__gallery-img-link" href="img/footer-instagram-5.jpg" data-fancybox="gallery">
+                                <img class="footer__gallery-img" src="img/footer-instagram-5.jpg" alt="Image">
+                            </a>
+                        </div>
+                        <div class="footer__gallery-img-wrap">
+                            <a class="footer__gallery-img-link" href="img/footer-instagram-6.jpg" data-fancybox="gallery">
+                                <img class="footer__gallery-img" src="img/footer-instagram-6.jpg" alt="Image">
+                            </a>
+                        </div>
+                        <div class="footer__gallery-img-wrap">
+                            <a class="footer__gallery-img-link" href="img/footer-instagram-7.jpg" data-fancybox="gallery">
+                                <img class="footer__gallery-img" src="img/footer-instagram-7.jpg" alt="Image">
+                            </a>
+                        </div>
+                        <div class="footer__gallery-img-wrap">
+                            <a class="footer__gallery-img-link" href="img/footer-instagram-8.jpg" data-fancybox="gallery">
+                                <img class="footer__gallery-img" src="img/footer-instagram-8.jpg" alt="Image">
+                            </a>
+                        </div>
+                        <div class="footer__gallery-img-wrap">
+                            <a class="footer__gallery-img-link" href="img/footer-instagram-9.jpg" data-fancybox="gallery">
+                                <img class="footer__gallery-img" src="img/footer-instagram-9.jpg" alt="Image">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer__copyright">
+            <p>&copy; 2016 MoGo free PSD template by <a href="javascript:;">Solaja</a></p>
+        </div>
+    </div>
 </footer>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
